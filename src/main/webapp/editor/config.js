f108875016af06475102626dafa6c77a2ea86823
mksmsr <mksmsr@gmail.com>
2014-02-06 00:47:32 +0530

    added editing article feature

---
+++ src/main/webapp/editor/config.js
@@ -0,0 +1,11 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	 config.language = 'ru';
+	 config.uiColor = '#5867A8';
+	 config.autoGrow_onStartup = true;
+};
