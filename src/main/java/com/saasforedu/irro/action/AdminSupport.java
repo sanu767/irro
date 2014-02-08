2f191ac34625dbf241e87e2b77af9cf05f542451
sankar <sanu767@gmail.com>
2014-02-08 11:27:39 +0530

    Commit admin support

---
+++ src/main/java/com/saasforedu/irro/action/AdminSupport.java
@@ -0,0 +1,13 @@
+package com.saasforedu.irro.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AdminSupport extends ActionSupport {
+
+	private static final long serialVersionUID = -215193160120223690L;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
