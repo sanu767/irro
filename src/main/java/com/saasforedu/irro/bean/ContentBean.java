5e9ff5cb97694113dc916b864f8c95e653cff443
sankar <sanu767@gmail.com>
2014-02-10 18:34:14 +0530

    content bean

---
+++ src/main/java/com/saasforedu/irro/bean/ContentBean.java
@@ -0,0 +1,15 @@
+package com.saasforedu.irro.bean;
+
+public class ContentBean {
+
+	private String mainContent;
+
+	public String getMainContent() {
+		return mainContent;
+	}
+
+	public void setMainContent(String mainContent) {
+		this.mainContent = mainContent;
+	}
+
+}
