4026f2dcc8494b4efdff69af33c0b91b7a458047
sankar <sanu767@gmail.com>
2014-02-13 00:43:57 +0530

    Actvities Service Commit

---
+++ src/main/java/com/saasforedu/irro/service/activities/IContestService.java
@@ -0,0 +1,10 @@
+package com.saasforedu.irro.service.activities;
+
+import com.saasforedu.irro.bean.ContentBean;
+
+public interface IContestService {
+
+	public String getMainContent();
+
+	public void updateContent(ContentBean content);
+}
---
+++ src/main/java/com/saasforedu/irro/service/activities/IOlympicService.java
@@ -0,0 +1,10 @@
+package com.saasforedu.irro.service.activities;
+
+import com.saasforedu.irro.bean.ContentBean;
+
+public interface IOlympicService {
+
+	public String getMainContent();
+
+	public void updateContent(ContentBean content);
+}
