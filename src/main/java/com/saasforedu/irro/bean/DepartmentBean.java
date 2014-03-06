2e1f7ff656a05eb94dd2aee3f0c53acd30a9206e
sankar <sanu767@gmail.com>
2014-03-06 16:58:30 +0600

    Commited Department Bean

---
+++ src/main/java/com/saasforedu/irro/bean/DepartmentBean.java
@@ -0,0 +1,42 @@
+package com.saasforedu.irro.bean;
+
+public class DepartmentBean {
+	
+	private String mainContent;
+	private String constitution;
+	private String contacts;
+	private String materials;
+	private String news;
+	
+	public String getMainContent() {
+		return mainContent;
+	}
+	public void setMainContent(String mainContent) {
+		this.mainContent = mainContent;
+	}
+	public String getConstitution() {
+		return constitution;
+	}
+	public void setConstitution(String constitution) {
+		this.constitution = constitution;
+	}
+	public String getContacts() {
+		return contacts;
+	}
+	public void setContacts(String contacts) {
+		this.contacts = contacts;
+	}
+	public String getMaterials() {
+		return materials;
+	}
+	public void setMaterials(String materials) {
+		this.materials = materials;
+	}
+	public String getNews() {
+		return news;
+	}
+	public void setNews(String news) {
+		this.news = news;
+	}
+	
+}
